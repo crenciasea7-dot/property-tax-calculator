df0fa42e14d7de3a3e338594de009b6abb508287
crenciasea7-dot <crenciasea7@gmail.com>
2026-08-05 15:43:20 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'property-tax-calculator-v1';
+const CACHE = 'property-tax-calculator-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
